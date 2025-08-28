a279837154ab41f5bc0a437479e4b7c5204661f1
bishwajit93 <bish.karm123@gmail.com>
2025-08-28 02:29:48 +0200

    basic setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-export default function Home() {
+// src/app/page.tsx
+export default function HomePage() {
   return (
-    <div>
-      <h1>
-        Hallo
-      </h1>
-    </div>
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl">Linda Güzel – Startseite</h1>
+    </main>
   );
 }
